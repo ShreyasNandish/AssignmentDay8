3101a465e075c64e8b17c1366bea3a541f2b0660
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-15 17:03:22 +0530

    Added: Program to check the 2 Lines

---
+++ src/com/bridgelabz/AssignmentDay8/LineComparsion.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.AssignmentDay8;
+
+public class LineComparsion {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison program ");
+        
+
+    }
+}
